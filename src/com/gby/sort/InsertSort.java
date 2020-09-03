2cc7fa246dc5934013bf4a2113e570250092a784
shaoping <shaoping@pinduoduo.com>
2020-09-03 20:41:37 +0800

    Update

---
+++ src/com/gby/sort/InsertSort.java
@@ -0,0 +1,30 @@
+package com.gby.sort;
+
+/**
+ * @author shaoping
+ * @date 2020/9/3
+ */
+public class InsertSort implements Sort {
+    @Override
+    public void sort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int cur = i;
+            while (cur > 0 && array[cur] < array[cur-1]) {
+                swap(array, cur, cur-1);
+                cur--;
+            }
+        }
+    }
+
+    public void sort2(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int cur = i;
+            while (cur > 0 && array[cur-1] > temp) {
+                array[cur] = array[cur-1];
+                cur--;
+            }
+            array[cur] = temp;
+        }
+    }
+}
